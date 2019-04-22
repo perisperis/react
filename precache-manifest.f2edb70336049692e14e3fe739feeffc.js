278db0d550702a57f6b2d6395d54648005e32eb1
Garcia Fernandez <pedro.garcia@atos.net>
2019-04-22 14:33:40 +0200

    Updates

---
+++ precache-manifest.f2edb70336049692e14e3fe739feeffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/perisperis/resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1de57982357126e19d12",
+    "url": "/perisperis/resume/static/js/runtime~main.7cae982e.js"
+  },
+  {
+    "revision": "90bd411cfda944de1bbd",
+    "url": "/perisperis/resume/static/js/main.03cbafa5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/perisperis/resume/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "90bd411cfda944de1bbd",
+    "url": "/perisperis/resume/static/css/main.a405239f.chunk.css"
+  },
+  {
+    "revision": "5598b942517779e43145fb68940c5e09",
+    "url": "/perisperis/resume/index.html"
+  }
+];
