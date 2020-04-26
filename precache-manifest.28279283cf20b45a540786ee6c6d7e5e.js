0b4a48d8eae756878fd354a5e7d5de293b749578
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-26 21:41:15 +0000

    Deploy perisperis/react to github.com/perisperis/react.git:gh-pages

---
+++ precache-manifest.28279283cf20b45a540786ee6c6d7e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "8e17f2c4b1caadd41814",
+    "url": "/react/static/js/main.534e7ed1.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8e17f2c4b1caadd41814",
+    "url": "/react/static/css/main.78f15556.chunk.css"
+  },
+  {
+    "revision": "95f111be6ddfc31bede7b21c703dc645",
+    "url": "/react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.217142cf10d3d10e0dc64badea0a3707.js"
+  "/react/precache-manifest.28279283cf20b45a540786ee6c6d7e5e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute("/react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
